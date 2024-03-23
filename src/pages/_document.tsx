2b4278a9411012e8a742a9808e910c03f220221a
Iulian Crisan <iuliancrisan01@gmail.com>
2024-03-23 15:23:59 +0200

    Home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body className="bg-gray-50 dark:bg-gradient-to-tr dark:from-slate-900 dark:via-slate-900 via-70% dark:to-slate-800  min-h-screen">
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
